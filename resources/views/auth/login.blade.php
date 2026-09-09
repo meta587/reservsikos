5a9c58ed3c718d7aad1ca512aa6038cf89929385
Meta Cahyani <metacahyani4@gmail.com>
2026-09-09 11:12:04 +0700

    update login.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -68,18 +68,14 @@
                                             Password
                                         </label>
 
-                                      <div class="password-wrapper">
-                                        <input 
-                                                type="password" 
-                                                name="password" 
-                                                id="password" 
-                                                class="form-control form-control-user @error('password') is-invalid @enderror" 
-                                                placeholder="Masukan password" 
-                                                required >
-                                        <button type="button" id="togglePassword" class="toggle-password">
-                                                👁️
-                                        </button>
-                                        </div>
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            id="password"
+                                            class="form-control form-control-user @error('password') is-invalid @enderror"
+                                            placeholder="Masukan password"
+                                            required
+                                        >
 
                                         @error('password')
                                             <div class="invalid-feedback d-block">
